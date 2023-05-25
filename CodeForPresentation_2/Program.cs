917f9a90af3f370eb5cd0f06704ae7b3440197c6
Dmitriy Aleksandrov <dmitriy_aleksandrov_2004@bk.ru>
2023-05-25 09:02:39 +0300

    Добавил новые файлы.(БМ)

---
+++ CodeForPresentation_2/Program.cs
@@ -0,0 +1,59 @@
+﻿using CodeForPresentation_2;
+using System.Diagnostics;
+using System.Text;
+
+internal class Program
+{
+    public static string GenerateRandomString(int length)
+    {
+        var builder = new StringBuilder();
+        var random = new Random();
+        for (int i = 0; i < length; i++)
+        {
+            var symbol = (char)random.Next(97,123);
+            builder.Append(symbol);
+        }
+        return builder.ToString();
+    }
+    public static bool DoRandomTest(int length1, int length2)
+    {
+        var tasker = new Tasker();
+        var time = new Stopwatch();
+        var source = GenerateRandomString(length1);
+        var pattern = GenerateRandomString(length2);
+        var sourceArr = source.ToArray();
+        var patternArr = pattern.ToArray();
+        time.Start();
+        var result1 = Tasker.IndexOf(sourceArr, patternArr);
+        time.Stop();
+        Console.WriteLine(time.ElapsedMilliseconds);
+        time.Reset();
+        time.Start();
+        var result2 = source.IndexOf(pattern);
+        time.Stop();
+        Console.WriteLine(time.ElapsedMilliseconds);
+        if (result1 != result2)
+        { 
+            Console.WriteLine(source);
+            Console.WriteLine(pattern);
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+        }
+        return result1 == result2;
+    }
+    private static void Main(string[] args)
+    {
+        bool isCorrect = true;
+        for (int i = 0; i < 10000; i++)
+        {
+            var random1 = new Random().Next(1,2500000);
+            var random2 = new Random().Next(1,random1);
+            if(!DoRandomTest(random1,random2))
+            {
+                Console.WriteLine(false);
+                isCorrect = false;
+            }
+        }
+        if (isCorrect) Console.WriteLine(true);
+    }
+}
---
+++ CodeForPresentation_2/Tasker.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeForPresentation_2
+{
+    public class Tasker
+    {
+        public static int IndexOf(char[] source, char[] pattern) 
+        {
+            if (source == null || pattern == null) throw new NullReferenceException();
+            if (source.Length < pattern.Length) return -1;
+            if (pattern.Length == 0) return 0;
+            var charTable = MakeCharTable(pattern); 
+            var offsetTable = MakeSuffixTable(pattern);
+            for (int i = pattern.Length - 1, j; i < source.Length;) 
+            { 
+                for (j = pattern.Length - 1; pattern[j] == source[i]; i--, j--) 
+                { 
+                    if (j == 0) 
+                    {
+                        return i; 
+                    } 
+                } 
+                i += Math.Max(offsetTable[pattern.Length - 1 - j], charTable[source[i]]); 
+            } 
+            return -1;
+        }
+        private static int[] MakeCharTable(char[] pattern) 
+        { 
+            int ALPHABET_SIZE = Char.MaxValue + 1;
+            int[] table = new int[ALPHABET_SIZE]; 
+            for (int i = 0; i < table.Length; ++i) 
+            { 
+                table[i] = pattern.Length; 
+            }
+            for (int i = 0; i < pattern.Length - 1; ++i) 
+            {
+                table[pattern[i]] = pattern.Length - 1 - i; 
+            } 
+            return table; 
+        }  
+        private static int[] MakeSuffixTable(char[] pattern) 
+        { 
+            var table = new int[pattern.Length]; 
+            var lastPrefixPosition = pattern.Length;
+            for (int i = pattern.Length; i > 0; --i) 
+            { 
+                if (IsPrefix(pattern, i))
+                { 
+                    lastPrefixPosition = i;
+                } 
+                table[pattern.Length - i] = lastPrefixPosition - i + pattern.Length;
+            } for (int i = 0; i < pattern.Length - 1; ++i) 
+            { 
+                var slen = SuffixLength(pattern, i); table[slen] = pattern.Length - 1 - i + slen; 
+            } 
+            return table;
+        }
+        private static bool IsPrefix(char[] pattern, int index) 
+        { 
+            for (int i = index, j = 0; i < pattern.Length; i++, j++)
+            { 
+                if (pattern[i] != pattern[j]) 
+                { 
+                    return false; 
+                } 
+            } 
+            return true;
+        } 
+        private static int SuffixLength(char[] needle, int index) 
+        { 
+            var length = 0; 
+            for (int i = index, j = needle.Length - 1; i >= 0 && needle[i] == needle[j]; i--, j--) 
+            { 
+                length += 1; 
+            }
+            return length; 
+        } 
+    }
+}
+
+
